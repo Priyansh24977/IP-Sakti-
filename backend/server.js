a69db7f50138a09641f772f2cf30bc337ea739e2
priyansh dwivedi <priyanshdwivedi15@gmail.com>
2026-09-17 09:02:14 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
-      
+      "https://ip-sakti-drab.vercel.app",
+      "http://localhost:5173",
     ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
